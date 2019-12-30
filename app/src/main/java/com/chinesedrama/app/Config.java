c85c51f2f19f499d4249ba8c8d2d144f2d7b426e
fando <fandofast@gmail.com>
2019-12-30 12:32:48 +0700

    Initial commit

---
+++ app/src/main/java/com/chinesedrama/app/Config.java
@@ -0,0 +1,20 @@
+package com.chinesedrama.app;
+
+
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "https://kdrama.website/cd/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "lovb6y5k3vv4soq5e7qfpghr";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://kdrama.website/cd/terms/";
+
+    //default theme
+    public static boolean DEFAULT_DARK_THEME_ENABLE = true;
+
+
+}
---
+++ app/src/main/java/com/chinesedrama/app/cn_network/apis/SearchApi.java
@@ -0,0 +1,15 @@
+package com.chinesedrama.app.cn_network.apis;
+
+import com.chinesedrama.app.cn_network.model.SearchModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SearchApi {
+
+    @GET("search")
+    Call<SearchModel> getSearchData(@Query("api_secret_key") String apiSecreteKey,
+                                    @Query("q") String query);
+
+}
